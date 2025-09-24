9d7232cd363bf91f5fd56058bccb92aef6fd9fa5
Atharva <105702198+AthxrvA7@users.noreply.github.com>
2025-09-24 17:16:18 +0530

    Create Q1&2.c

---
+++ Q1&2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+  int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("Their sum is %d\n", a+b);
+    printf("Their difference is %d\n", a-b);
+    printf("Their product is %d\n", a*b);
+    printf("Their quotient is %d\n", a/b);
+  }
