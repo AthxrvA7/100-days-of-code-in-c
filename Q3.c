f250b252fa89c04bb6aaf9ab0e29012698e3fe26
Atharva <105702198+AthxrvA7@users.noreply.github.com>
2025-09-24 17:20:38 +0530

    Create Q3.c

---
+++ Q3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+  int main() {
+    int per, area, len, wid;
+    printf("Enter length and width of rectangle: ");
+    scanf("%d %d", &len, &wid);
+    area = len * wid;
+    per = 2 * (len + wid);
+    printf("Area and perimeter of rectangle is %d and %d.\n", area, per);
+    
+  }
