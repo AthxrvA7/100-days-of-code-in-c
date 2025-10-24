f2af70b3cb485bc31938c6b41833a02a4ae5f248
Atharva <105702198+AthxrvA7@users.noreply.github.com>
2025-10-24 15:10:14 +0530

    Create Q13.c

---
+++ Q13.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+  int year;
+  printf("Enter a year: ");
+  scanf("%d", &year);
+  if (year % 4 == 0)
+    printf("This is a leap year.\n");
+  else
+    printf("This is not a leap year.\n");
+};
