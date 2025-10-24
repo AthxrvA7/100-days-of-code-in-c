938f1d4c4055a396b333ba2238e69f56da8b3ff1
Atharva <105702198+AthxrvA7@users.noreply.github.com>
2025-10-24 15:15:45 +0530

    Create Q14.c

---
+++ Q14.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+  char ch;
+  printf("Enter a character: ");
+  scanf("%c", &ch);
+  if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+    printf("The character is a vowel.\n");
+  else
+    printf("The character is a consonant.\n");
+};
