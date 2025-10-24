544d1bb0addfbeeb2fdc6fa92a1e7d981226a94d
Atharva <105702198+AthxrvA7@users.noreply.github.com>
2025-10-24 16:50:06 +0530

    Create Q31.c

---
+++ Q31.c
@@ -0,0 +1,20 @@
+// Write a program to take a number as input and print its equivalent binary representation.
+
+#include <stdio.h>
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    int binary[32];
+    int i = 0;
+    while (num > 0) {
+        binary[i] = num % 2;
+        num = num / 2;
+        i++;
+    }
+    for (int j = i - 1; j >= 0; j--) {
+        printf("%d", binary[j]);
+    }
+    printf("\n");
+};
