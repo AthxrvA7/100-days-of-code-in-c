2038c18b506c452a68444cccd3b6a65151454d6f
Atharva <105702198+AthxrvA7@users.noreply.github.com>
2025-10-24 15:04:02 +0530

    Create Q11.c

---
+++ Q11.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+  int num;
+  printf("Enter a number: ");
+  scanf("%d", &num);
+  if ( num % 2 == 0 ) {
+    printf("The number is even.\n");
+  }
+  else {
+    printf("The number is odd.\n");
+  };
+};
